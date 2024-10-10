97821a7fe7255727e50bd8f8cc42eb23d72f8762
Hanzeel Villa <hanzeelvilla0@gmail.com>
2024-10-09 19:54:36 -0600

    crud + mostrar canal

--- app/src/main/java/com/hanzeel/iptvandroidstudio/MainActivity.kt
+++ app/src/main/java/com/hanzeel/iptvandroidstudio/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.hanzeel.iptvandroidstudio
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.PlayerView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import android.widget.Toast
 import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var channelAdapter: RecyclerAdapter
     private var channelList: MutableList<Channel> = mutableListOf()
+    private lateinit var playerView: PlayerView
+    private lateinit var player: ExoPlayer
 
     // Registrar el ActivityResultLauncher para manejar el resultado del EditChannelActivity
     private val editChannelLauncher = registerForActivityResult(
@@ -47,17 +53,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Inicializar el reproductor
+        playerView = findViewById(R.id.player_view)
+        player = ExoPlayer.Builder(this).build()
+        playerView.player = player
+
         // Inicializar RecyclerView y el adaptador
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         // Inicializar la lista de canales
-        channelAdapter = RecyclerAdapter(channelList) { selectedChannel ->
+        channelAdapter = RecyclerAdapter(channelList, { selectedChannel ->
             openEditChannelActivity(selectedChannel)
-        }
+        }, { channelToChange -> // Manejar el clic en "Cambiar Canal"
+            changeChannel(channelToChange)
+        })
+
         recyclerView.adapter = channelAdapter
 
-        // Cargar la lista de canales (esto es solo un ejemplo, puede ser desde una base de datos o API)
+        // Cargar la lista de canales
         loadChannels()
     }
 
@@ -75,7 +89,6 @@ class MainActivity : AppCompatActivity() {
         if (index != -1) {
             channelList[index] = updatedChannel
             channelAdapter.notifyItemChanged(index)
-            Toast.makeText(this, "Canal actualizado: ${updatedChannel.name}", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -85,6 +98,30 @@ class MainActivity : AppCompatActivity() {
         channelList.add(Channel(1, "Pequeradio TV", "Infantil", "https://canadaremar2.todostreaming.es/live/peque-pequetv.m3u8", "https://static.wixstatic.com/media/76b12f_b725806aac4c416da697ccf6a5c6dd83~mv2.png/v1/fit/w_2500,h_1330,al_c/76b12f_b725806aac4c416da697ccf6a5c6dd83~mv2.png"))
         channelList.add(Channel(2, "Red bull TV", "Deportes", "https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master.m3u8", "https://image.roku.com/developer_channels/prod/0560cd3757ba28d0525e524fe0d98c1b95721c5ecf5fda464eba3084eeed7f36.png"))
         channelList.add(Channel(3, "AKC TV Dogs", "Animales", "https://install.akctvcontrol.com/speed/broadcast/138/desktop-playlist.m3u8", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLPq9IAKaYCAvNxg2AWKukgyAtvBbMa9GVZQ&s"))
+        channelList.add(Channel(4, "Pequeradio TV", "Infantil", "https://canadaremar2.todostreaming.es/live/peque-pequetv.m3u8", "https://static.wixstatic.com/media/76b12f_b725806aac4c416da697ccf6a5c6dd83~mv2.png/v1/fit/w_2500,h_1330,al_c/76b12f_b725806aac4c416da697ccf6a5c6dd83~mv2.png"))
+        channelList.add(Channel(5, "Red bull TV", "Deportes", "https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master.m3u8", "https://image.roku.com/developer_channels/prod/0560cd3757ba28d0525e524fe0d98c1b95721c5ecf5fda464eba3084eeed7f36.png"))
+        channelList.add(Channel(6, "AKC TV Dogs", "Animales", "https://install.akctvcontrol.com/speed/broadcast/138/desktop-playlist.m3u8", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLPq9IAKaYCAvNxg2AWKukgyAtvBbMa9GVZQ&s"))
+        channelList.add(Channel(7, "Pequeradio TV", "Infantil", "https://canadaremar2.todostreaming.es/live/peque-pequetv.m3u8", "https://static.wixstatic.com/media/76b12f_b725806aac4c416da697ccf6a5c6dd83~mv2.png/v1/fit/w_2500,h_1330,al_c/76b12f_b725806aac4c416da697ccf6a5c6dd83~mv2.png"))
+        channelList.add(Channel(8, "Red bull TV", "Deportes", "https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master.m3u8", "https://image.roku.com/developer_channels/prod/0560cd3757ba28d0525e524fe0d98c1b95721c5ecf5fda464eba3084eeed7f36.png"))
+        channelList.add(Channel(9, "AKC TV Dogs", "Animales", "https://install.akctvcontrol.com/speed/broadcast/138/desktop-playlist.m3u8", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLPq9IAKaYCAvNxg2AWKukgyAtvBbMa9GVZQ&s"))
+        channelList.add(Channel(10, "Pequeradio TV", "Infantil", "https://canadaremar2.todostreaming.es/live/peque-pequetv.m3u8", "https://static.wixstatic.com/media/76b12f_b725806aac4c416da697ccf6a5c6dd83~mv2.png/v1/fit/w_2500,h_1330,al_c/76b12f_b725806aac4c416da697ccf6a5c6dd83~mv2.png"))
+        channelList.add(Channel(11, "Red bull TV", "Deportes", "https://rbmn-live.akamaized.net/hls/live/590964/BoRB-AT/master.m3u8", "https://image.roku.com/developer_channels/prod/0560cd3757ba28d0525e524fe0d98c1b95721c5ecf5fda464eba3084eeed7f36.png"))
+        channelList.add(Channel(12, "AKC TV Dogs", "Animales", "https://install.akctvcontrol.com/speed/broadcast/138/desktop-playlist.m3u8", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLPq9IAKaYCAvNxg2AWKukgyAtvBbMa9GVZQ&s"))
+
         channelAdapter.notifyDataSetChanged() // Notificar cambios al adaptador
+    }
+
+    // Método para cambiar el canal
+    private fun changeChannel(channel: Channel) {
+        // Configurar el reproductor para reproducir el canal seleccionado
+        val mediaItem = MediaItem.fromUri(Uri.parse(channel.link))
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        player.release() // Liberar el reproductor cuando la actividad se detiene
     }
 }
--- app/src/main/java/com/hanzeel/iptvandroidstudio/RecyclerAdapter.kt
+++ app/src/main/java/com/hanzeel/iptvandroidstudio/RecyclerAdapter.kt
@@ -3,41 +3,53 @@ package com.hanzeel.iptvandroidstudio
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
-class RecyclerAdapter(private val channelList: List<Channel>, private val onChannelClick: (Channel) -> Unit) : RecyclerView.Adapter<RecyclerAdapter.ChannelViewHolder>() {
-
-    class ChannelViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val nameTextView: TextView = view.findViewById(R.id.textViewName)
-        val categoryTextView: TextView = view.findViewById(R.id.textViewCategory)
-        val channelImageView: ImageView = view.findViewById(R.id.imageView)
-    }
+class RecyclerAdapter(
+    private val channelList: MutableList<Channel>,
+    private val onChannelClick: (Channel) -> Unit,
+    private val onChangeChannelClick: (Channel) -> Unit // Nuevo parámetro para manejar el clic en el botón
+) : RecyclerView.Adapter<RecyclerAdapter.ChannelViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChannelViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.channel_item, parent, false)
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.channel_item, parent, false)
         return ChannelViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ChannelViewHolder, position: Int) {
         val channel = channelList[position]
-        holder.nameTextView.text = channel.name
-        holder.categoryTextView.text = channel.category
+        holder.bind(channel)
 
-        Glide.with(holder.itemView.context)
-            .load(channel.image)
-            .into(holder.channelImageView)
+        // Manejar el clic en el canal
+        holder.itemView.setOnClickListener { onChannelClick(channel) }
 
-        // Manejar el clic en un canal
-        holder.itemView.setOnClickListener {
-            onChannelClick(channel)
+        // Manejar el clic en el botón "Cambiar Canal"
+        holder.changeChannelButton.setOnClickListener {
+            onChangeChannelClick(channel) // Llamar al método cuando se hace clic en el botón
         }
     }
 
-    override fun getItemCount(): Int {
-        return channelList.size
+    override fun getItemCount(): Int = channelList.size
+
+    inner class ChannelViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView: ImageView = itemView.findViewById(R.id.imageView)
+        private val textViewName: TextView = itemView.findViewById(R.id.textViewName)
+        private val textViewCategory: TextView = itemView.findViewById(R.id.textViewCategory)
+        val changeChannelButton: Button = itemView.findViewById(R.id.change_channel_button)
+
+        fun bind(channel: Channel) {
+            // Configurar el canal en la vista
+            textViewName.text = channel.name
+            textViewCategory.text = channel.category
+            // Cargar la imagen en imageView usando Glide
+            Glide.with(itemView.context)
+                .load(channel.image)
+                .into(imageView)
+        }
     }
 }
-
